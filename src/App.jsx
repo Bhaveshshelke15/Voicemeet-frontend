48f2b995e0f1ae508e5fbc7f727429706307ef26
Bhavesh <bhaveshsshelke777@gmail.com>
2026-03-21 00:04:55 +0530

    fix route issue hashrouter import

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+//import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import AdminLogin from "./pages/admin/AdminLogin";
