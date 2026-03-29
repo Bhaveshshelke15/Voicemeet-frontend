cb8fbe4b3325bd9d96a57ce97f1e788071a35f7d
Bhavesh <bhaveshsshelke777@gmail.com>
2026-03-29 10:23:13 +0530

    traffic axiosconfig update

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,21 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://voicemeet.onrender.com"
+  baseURL: "https://voicemeet.onrender.com",
+  timeout: 15000, // 🔥 important for mobile
 });
 
 API.interceptors.request.use((config) => {
-
-  const token =
-    localStorage.getItem("adminToken") ||
-    localStorage.getItem("userToken");
+  const token = window.localStorage.getItem("token"); // safer for mobile
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
-
 });
 
 export default API;
